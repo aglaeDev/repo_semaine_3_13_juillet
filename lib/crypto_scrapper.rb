4b7f3ee05b9867ceb127903bddd454ecc4e4b304
aglaeDev <a.gourdet.dev@gmail.com>
2023-07-14 10:42:48 +0200

    test

--- lib/crypto_scrapper.rb
+++ lib/crypto_scrapper.rb
@@ -11,15 +11,12 @@ def crypto_scrapper
 
     doc.xpath('/html/body/div[1]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').each do |link|
         cryto_name<<link.content
-        #crypto << cryto_name
     end
 
     doc.xpath('/html/body/div[1]/div[2]/div[2]/div/div[1]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/div').each do |link|
         cryto_price<<link.content
-        #crypto << cryto_price
     end
 
     arr = cryto_name.zip(cryto_price).map {|name, price|{name => price}}
-    #crypto << arr
     return arr
 end
